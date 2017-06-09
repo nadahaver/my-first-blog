04c01cbe1d8a49c609c2b83f1e81ac4f8b6727c6
carlos <alemaokdu09@gmail.com>
2017-06-09 18:39:21 -0300

    Modified templates to display posts from database.

--- blog/views.py
+++ blog/views.py
@@ -4,6 +4,7 @@ from .models import Post
 from django.shortcuts import render, get_object_or_404
 from .forms import PostForm
 
+
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
@@ -13,3 +14,14 @@ def post_detail(request, pk):
 def post_new(request):
     form = PostForm()
     return render(request, 'blog/post_edit.html', {'form': form})
+if request.method == "POST":
+    [form = PostForm(request.POST)]
+    if form.is_valid():
+    post = form.save(commit=False)
+    post.author = request.user
+    post.published_date = timezone.now()
+    post.save()
+else:
+    form = PostForm()
+return render(request, 'blog/post_edit.html', {'form': form})
+
